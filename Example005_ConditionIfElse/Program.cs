e907f6d1b226a7cfe4473246a3dd86b773486d01
Nastya Piunova <piunovaam@yandex.ru>
2023-01-27 19:30:13 +0300

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "кирилл")
+{
+    Console.WriteLine("Ура, это же Кирилл!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

3203e27adc679c3420a3bbed98f2a90fefb65896
Nastya Piunova <piunovaam@yandex.ru>
2023-02-09 22:32:28 +0300

    Example 012 Methods

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,111 @@
+﻿//Вид 1
+/*
+void Method1() //здесь в скобках мы можем и не указывать ничего в качестве аргументов 
+{
+    Console.WriteLine("Автор: Пиунова Анастасия");
+}
+
+Method1(); // Вызываем метод, только со скобками
+// void принимает данные, но не возвращает их
+*/
+//Вид 2
+/*
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Текст сообщения");
+
+void Method21(string msg,int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(msg: "Текст", count: 4);
+*/
+//Вид 3
+/*
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+*/
+//Вид 4
+/*
+int Method4(string count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(count: 10, text: "sweety");
+Console.WriteLine(res);
+
+int Method4(string count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(count: 10, text: "sweety");
+Console.WriteLine(res);
+*/
+//Цикл for
+/*
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+*/
+// Работа с текстом
+//Дан текст. В тексте нужно все пробелы заменить черточками
+//маленькие буквы "к" заменить большими "К",
+//а большие буквы "С" заменить маленькими "с"
+
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = "qwerty"
+//             012345
+// s[3] = r то есть третий от нуля символ 
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
